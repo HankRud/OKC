fb0136768a8c2930ce9036500e5ee04d86f58967
Rudboi <69220761+Rudboi@users.noreply.github.com>
2022-01-04 10:56:24 -0800

    Add files via upload

--- OKC_Asses.py
+++ OKC_Asses.py
@@ -9,6 +9,7 @@
 ##area for each team
 ##all assumptions about court dimensions are taken from
 ##https://proformancehoops.com/basketball-court-dimensions/
+##percentages are reported as decimal values
 
 #Import CSV module
 import csv
@@ -18,29 +19,29 @@ import math
 #declare   variables for team A and team B
 
 
-#total Corner 3's
-cornerA = 0
-cornerB = 0
+#total_al Corner 3's
+corner_attempted_a = 0
+corner_attempted_b = 0
 
 #Corner 3's made
-cornerMadeA = 0
-cornerMadeB = 0
+corner_made_a = 0
+corner_made_b = 0
 
-#total NC3's
-nc3A = 0
-nc3B = 0
+#total_al NC3's
+nc3_attempted_a = 0
+nc3_attempted_b = 0
 
 #NC3 made
-nc3MadeA = 0 
-nc3MadeB = 0
+nc3_made_a = 0 
+nc3_made_b = 0
 
-#total two's
-twoA = 0
-twoB = 0
+#total_al two's
+two_attempted_a = 0
+two_attempted_b = 0
 
 #two's made
-twoMadeA = 0
-twoMadeB = 0
+two_made_a = 0
+two_made_b = 0
 
 
 #class driver
@@ -64,140 +65,145 @@ def main():
             mk = float(make)
             shotCounter(team,xcoord,ycoord,mk)
 
-    #Print EFG for Corner Threes
+    #sum total_als of all attempts
+    total_a = nc3_attempted_a + corner_attempted_a + two_attempted_a
+    total_b = nc3_attempted_b + corner_attempted_b + two_attempted_b
+
+    #Print effective_field_goal for Corner Threes 
     print("Corner Three effective field goal percentages: \n\n")
-    print("Team A: " + eFG(cornerMadeA,0,cornerA) + "\n")
-    print("Team B: " + eFG(cornerMadeB,0,cornerB ) + "\n")
+    print_ab(effective_field_goal(corner_made_a,0,corner_attempted_a),effective_field_goal(corner_made_b,0,corner_attempted_b ))
 
-    #Print EFG for Threes
+    #Print effective_field_goal for Threes
     print("Non-Corner Three effective field goal percentages: \n\n")
-    print("Team A: " + eFG(nc3MadeA,0,nc3A) + "\n")
-    print("Team B: " + eFG(nc3MadeB,0,nc3B) + "\n")
+    print_ab(effective_field_goal(nc3_made_a,0,nc3_attempted_a),effective_field_goal(nc3_made_b,0,nc3_attempted_b))
 
-    #Print EFG for Twos
+    #Print effective_field_goal for Twos
     print("Two point effective field goal percentages: \n\n")
-    print("Team A: " + eFG(twoMadeA,0,twoA) + "\n")
-    print("Team B: " + eFG(twoMadeB,0,twoB) + "\n\n")
+    print_ab(effective_field_goal(0,two_made_a,two_attempted_a),effective_field_goal(0,two_made_b,two_attempted_b))
 
-    totA = nc3A + cornerA + twoA
-    totB = nc3B + cornerB + twoB
 
     #Print Percentage Shot Distribution for Corner Threes
     print("Shot Distribution for Corner Threes:  \n\n")
-    print("Team A: " + shotDistribution(cornerA, totA) + "\n")
-    print("Team B: " + shotDistribution(cornerB, totB) + "\n")
+    print_ab(shot_distribution(corner_attempted_a, total_a), shot_distribution(corner_attempted_b, total_b))
 
     #Print Percentage Shot Distribution for Non-Corner Threes\  
     print("Shot Distribution for Non-Corner Threes:  \n\n")
-    print("Team A: " + shotDistribution(nc3A, totA) + "\n")
-    print("Team B: " + shotDistribution(nc3B, totB) + "\n")
+    print_ab(shot_distribution(nc3_attempted_a, total_a), shot_distribution(nc3_attempted_b, total_b) )
 
     #Print Percentage Shot Distribution for Twos
     print("Shot Distribution for Two:  \n\n")
-    print("Team A: " + shotDistribution(twoA, totA ) + "\n")
-    print("Team B: " + shotDistribution(twoB, totB ) + "\n")
+    print_ab(shot_distribution(two_attempted_a, total_a), shot_distribution(two_attempted_b, total_b ))
 
         
 
 #the shotCounter adds the shot attempt to the correct   if the
 #attempt is taken within the regulation NBA court                     
 def shotCounter(team,x,y, make):
-    global cornerA
-    global cornerMadeA
-    global nc3A
-    global nc3MadeA
-    global twoA
-    global twoMadeA
-    global cornerB
-    global cornerMadeB
-    global nc3B
-    global nc3MadeB
-    global twoB
-    global twoMadeB
+    global corner_attempted_a
+    global corner_made_a
+    global nc3_attempted_a
+    global nc3_made_a
+    global two_attempted_a
+    global two_made_a
+    global corner_attempted_b
+    global corner_made_b
+    global nc3_attempted_b
+    global nc3_made_b
+    global two_attempted_b
+    global two_made_b
 
     
     #determine if attempt in bounds
-    if isInBounds(x,y):
+    if is_in_bounds(x,y):
         #for teamA
         if team == "Team A":
             #if Corner3 add to corner3 counter
-            if isCorner(x,y):
-                cornerA += 1
+            if is_corner(x,y):
+                corner_attempted_a += 1
                 #if make add to corner3 make counter
                 if make == 1:
-                    cornerMadeA += 1
+                    corner_made_a += 1
             #if NC3 add to NC3 counter   
-            elif isNC3(x,y):
-                nc3A += 1
+            elif is_nc3(x,y):
+                nc3_attempted_a += 1
                 if make == 1:
-                      nc3MadeA += 1
+                      nc3_made_a += 1
             else:
-                twoA += 1
+                #otherwise shot is a two 
+                two_attempted_a += 1
                 if make == 1:
-                      twoMadeA += 1
+                      two_made_a += 1
         #for teamB             
         else:
         
-            if isCorner(x,y):
-                cornerB+= 1
+            if is_corner(x,y):
+                corner_attempted_b+= 1
                 if make == 1:
-                      cornerMadeB += 1
-            elif isNC3(x,y):
-                nc3B += 1
+                      corner_made_b += 1
+            elif is_nc3(x,y):
+                nc3_attempted_b += 1
                 if make == 1:
-                      nc3MadeB += 1
+                      nc3_made_b += 1
             else:
-                twoB += 1
+                two_attempted_b += 1
                 if make == 1:
-                    twoMadeB += 1 
+                    two_made_b += 1 
                 
             
 
-#isInBounds parameters:  x coord, y coord
+#is_in_bounds parameters:  x coord, y coord
 #return: true if a shot is taken within the bounds of the regulation
 #NBA court
-def isInBounds(x,y):
+def is_in_bounds(x,y):
     if ((x >= -25 and x <= 25) and (y >= -4.75 and y <= 89.25)):
         return True
     else:
         return False
 
-#isCorner parameters: x coord, y coord
+#is_corner parameters: x coord, y coord
 #return: true if a shot is a Corner 3 (y<= 7.8)
-def isCorner(x,y):
+def is_corner(x,y):
     if (y<= 7.8 and abs(x)> 22.0):
         return True
     else:
         return False
 
-#isNC3  parameters: x coord, y coord
+#is_nc3  parameters: x coord, y coord
 #return : true if shot is a non-corner three
-def isNC3(x,y):
+def is_nc3(x,y):
     c = math.sqrt((x*x) + (y*y))
     if (c > 23.75):
         return True
     else:
         return False
 
-#eFG parameters: three pointers made, two pointers made, field goals attempted
+#effective_field_goal parameters: three pointers made, two pointers made, field goals attempted
 #return : the effective field goal percentage rounded to three decimal places
-def eFG(threePM,twoPM,fGA):
-    if (fGA > 0):
-        efg = (((threePM+twoPM) + (.5*threePM))/fGA)*100
-        formatter = "{0:10.5}"
-        output= formatter.format(efg)
-        return output
-    else: return "0"
+def effective_field_goal(three_pointers_made,two_pointers_made,field_goal_attempts):
+    if (field_goal_attempts > 0):
+        effective_field_goal = (((three_pointers_made+two_pointers_made) + (.5*three_pointers_made))/field_goal_attempts)
 
-#shotDistribution parameters # of attempts for type, total number of attempts
-def shotDistribution(typeAttempts,attempts):
+        return round(effective_field_goal,3)
+    else: return 0
+
+#shot_distribution parameters # of attempts for type, total_al number of attempts
+def shot_distribution(type_attempts,attempts):
     if (attempts > 0):
-        dist = (typeAttempts/attempts) * 100
-        formatter = "{0:10.5}"
-        output = formatter.format(dist)
-        return output
-    else: return "0"
+        distribution = (type_attempts/attempts)
         
+        return round(distribution,3)
+    else: return 0
+
+#takes floats of results for each team and prints them
+def print_ab(team_a_result,team_b_result):
+    #print team a reasults
+    print("Team A: ")
+    print(team_a_result)
+    print("\n")
+    
+    print("Team B: ")
+    print(team_b_result)
+    print("\n")
 
 main()
     
